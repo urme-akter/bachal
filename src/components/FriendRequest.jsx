1632ccb6bebef2ec49e5c45e38c78b12e066f7a0
urme-akter <112346871+urme-akter@users.noreply.github.com>
2023-07-13 00:36:25 +0600

    show friend request list

--- src/components/FriendRequest.jsx
+++ src/components/FriendRequest.jsx
@@ -1,47 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import profile from "../assets/profile.png";
 import { Button } from "@mui/material";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector } from "react-redux";
 
 const FriendRequest = () => {
+  const db = getDatabase();
+  let [reqList, setReqList] = useState([]);
+  let userData = useSelector((state) => state.loggedUser.loginUser);
+
+  useEffect(() => {
+    const friendRequestRef = ref(db, "friendrequest");
+    onValue(friendRequestRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (item.val().whoreceiveid == userData.uid) {
+          arr.push(item.val());
+        }
+      });
+      setReqList(arr);
+    });
+  }, []);
+
   return (
-    <div className="groupBox">
+    <div className="groupBox ">
       <h3>Friend Request</h3>
-      <div className="list">
-        <div className="img">
-          <img src={profile} className="pic" />
+      {reqList.map((item) => (
+        <div className="list" key={item.id}>
+          <div className="img">
+            <img src={profile} className="pic" />
+          </div>
+          <div className="details">
+            <h4 className="">{item.whosendname}</h4>
+            <p>Hi Guys, Wassup!</p>
+          </div>
+          <div className="button">
+            <Button variant="contained">Join</Button>
+          </div>
         </div>
-        <div className="details">
-          <h4 className="">Friends Reunion</h4>
-          <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-          <Button variant="contained">Join</Button>
-        </div>
-      </div>
-      <div className="list">
-        <div className="img">
-          <img src={profile} className="pic" />
-        </div>
-        <div className="details">
-          <h4 className="">Friends Reunion</h4>
-          <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-          <Button variant="contained">Join</Button>
-        </div>
-      </div>
-      <div className="list">
-        <div className="img">
-          <img src={profile} className="pic" />
-        </div>
-        <div className="details">
-          <h4 className="">Friends Reunion</h4>
-          <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-          <Button variant="contained">Join</Button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
